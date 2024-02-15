62cd6b7e6898cd92f2dcbff64df68ad00bb6b1d9
Ash <hello@ashconnell.com>
2024-02-15 12:43:55 +1100

    stuff

---
+++ packages/firebolt-docs/components/Account.js
@@ -0,0 +1,13 @@
+import { useData } from 'firebolt'
+
+export default function Account() {
+  const data = useData(getAccount)
+  const account = data.get()
+  return <div onClick={() => data.invalidate()}>Name: {account.name}</div>
+}
+
+export async function getAccount(req) {
+  req.expire(1000)
+  await new Promise(resolve => setTimeout(resolve, 1000))
+  return { name: 'Jim_' + Math.random() }
+}
---
+++ packages/firebolt-docs/pages/account.js
@@ -0,0 +1,24 @@
+import { Suspense, useState } from 'react'
+import { useData, useAction } from 'firebolt'
+import Account from '../components/Account'
+
+export default function Page() {
+  const data = useData(getThing)
+  const thing = data.get()
+
+  return (
+    <div>
+      <div>{thing}</div>
+      <Account />
+      <Account />
+      <Account />
+      <Account />
+      <Account />
+      <Account />
+    </div>
+  )
+}
+
+export async function getThing() {
+  return 'THing!'
+}
