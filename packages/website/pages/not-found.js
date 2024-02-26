c4682d3e80493ec8bfd9da5f253f2eb53a28c931
Ash <hello@ashconnell.com>
2024-02-26 23:22:37 +1100

    [website] use Metadata in not-found

--- packages/website/pages/not-found.js
+++ packages/website/pages/not-found.js
@@ -1,3 +1,10 @@
+import { Metadata } from '../components/Metadata'
+
 export default function NotFound() {
-  return <div>Not Found</div>
+  return (
+    <>
+      <Metadata title='Not Found' />
+      <div>Not Found</div>
+    </>
+  )
 }
