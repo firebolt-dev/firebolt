37ec705e711fb73626539414b73095515845c607
Ash <hello@ashconnell.com>
2024-03-03 23:20:10 +1100

    [website] pretty /not-found

--- packages/website/routes/not-found.js
+++ packages/website/routes/not-found.js
@@ -1,10 +1,32 @@
+import { css } from 'firebolt'
+
 import { Metadata } from '@/components/Metadata'
 
 export default function NotFound() {
   return (
     <>
-      <Metadata title='Not Found' />
-      <div>Not Found</div>
+      <Metadata title='Page not found' />
+      <div
+        css={css`
+          height: 100vh;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          h1 {
+            font-size: 32px;
+            font-weight: 700;
+            margin: 0 0 12px;
+          }
+          p {
+            border-top: 1px solid var(--line-color);
+            padding: 12px 0 0;
+          }
+        `}
+      >
+        <h1>404</h1>
+        <p>Page not found</p>
+      </div>
     </>
   )
 }
